382876096cd4579f9e8ed48c4aa16b90d354cd60
khunaungphyo8800@gmail.com <aungphyo9900@outlook.com>
2024-08-24 14:47:32 +0700

    blank

--- src/components/SkillPage.vue
+++ src/components/SkillPage.vue
@@ -3,13 +3,13 @@
         <div class="skills flex items-center gap-10 md:mt-20 mt-10 mb-10">
             <div class=" skills-box flex gap-3 items-center">
                 <div @click="download" class="html flex cursor-pointer hover:translate-y-[-5px] duration-200 ease-in">
-                    <a href="https://gitlab.com/ayemya5500/404vpn/-/raw/main/404VPN_1.1.3.apk">
+                    <a href="https://gitlab.com/ayemya5500/404vpn/-/raw/main/404VPN_1.1.3.apk" target="_blank">
                         <img class=" w-[200px] " src="../assets/apk-direct-download.png" alt="">
                     </a>
                 </div>
                 <ToastMine ref="toast" message="Downloading..." />
                 <div @click="play" class="php flex cursor-pointer hover:translate-y-[-5px] duration-200 ease-in">
-                    <a href="https://gitlab.com/ayemya5500/404vpn/-/raw/main/404VPN_1.1.3.apk">
+                    <a href="https://gitlab.com/ayemya5500/404vpn/-/raw/main/404VPN_1.1.3.apk" target="_blank">
                         <img class=" w-[200px] " src="../assets/playstore.png" alt="">
                     </a>
                 </div>
